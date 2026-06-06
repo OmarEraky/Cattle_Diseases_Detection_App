f7f61b0f9a455caf216dbafdc54014ae9d583aea
Omar <omar@debian.Omar>
2026-06-06 23:06:39 +0300

    Wipe all files from repository

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:cattle_disease_app/app.dart';
-
-void main() {
-  // Ensure that plugin services are initialized before running the app
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  runApp(const CattleDiseaseApp());
-}
